033d3fb3b283cf238e9ac0d9626611fc9f3a1eaf
bahodirxon <bohodirxon.mahmudoov@gmail.com>
2025-05-31 14:54:46 +0500

    funksiyalar

---
+++ app.js
@@ -0,0 +1,52 @@
+// Sonni 2 ga Ko'paytirib Beradi
+
+function twoTimes(x) {
+  return x * 2;
+}
+// console.log(twoTimes(5));
+
+// Ob Havoni Hisoblaydi
+
+function toCelsius(f) {
+  return ((f - 32) * 5) / 9;
+}
+// console.log(toCelsius(100));
+
+// Juft Yoki Toqligini Aniqlaydi
+
+function isEven(num) {
+  return num % 2 === 0;
+}
+// console.log(isEven(2));
+
+// Toq Sonni Aniqlovchi
+
+function isOdd(num) {
+  return num % 2 === 1;
+}
+// console.log(isOdd(3));
+
+// Tepadagi Funksiyani If Else Bilan Yozilishi
+
+function issOdd(num) {
+  if (num % 2 === 1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+// console.log(isOdd(3));
+
+// Berilgan Sonni Kvadratini Hisoblaydi
+
+function square(num) {
+  return num * num;
+}
+// console.log(square(10));
+
+// Foydalanuchi Ismini Olib Unga Salom Beradigan
+
+function greetUser(name) {
+  return "Salom, " + name + "!";
+}
+// console.log(greetUser("Bahodirxon"));
